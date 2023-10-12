25f458db4434d6d1416a5c0b175c568c924a8c4c
sogonov <sogonove@yandex.ru>
2023-10-12 11:33:57 +0400

    <update>

---
+++ ads1118.c
@@ -0,0 +1,86 @@
+﻿struct ADS1118_Config_t configReg1118 ;
+
+void ads1118_config(void)
+{
+	configReg1118.operatingMode					=	SCS;
+    configReg1118.channel						=	AIN0_AIN1;
+	configReg1118.pgaConfig				    =	FS4096;
+	configReg1118.modeConfig				=	PDSSM;
+	configReg1118.dataRate						=	SPS250;
+	configReg1118.ADC_TSmode						=	ADC_MODE;
+	configReg1118.pullMode						=	PULL_UP_DIS;
+	configReg1118.validMode						=	DATA_VALID;   
+	configReg1118.readyMode                    = DATA_READY;
+}
+
+
+void ADS1118_SetDefault(ADS1115_Handle_t *pConfig){
+	configReg1118.operatingMode					=	SCS;
+    configReg1118.channel						=	AIN0_AIN1;
+	configReg1118.pullMode						=	PULL_UP_DIS;
+	configReg1118.readyMode                    = DATA_READY;
+	
+	configReg1118.ADC_TSmode						=	ADC_MODE;
+	configReg1118.validMode						=	DATA_VALID;   
+
+	configReg1118.pgaConfig				    =	FS4096;
+	configReg1118.dataRate						=	SPS250;
+	configReg1118.modeConfig				=	CCM;
+
+
+	prepareConfigFrame(bytes, configReg1118);
+
+}
+
+
+
+void SPI_config(void)
+{
+	
+}
+
+
+float raw_to_voltage(
+		uint16_t raw,
+		ADS1118_Handle_t *pConfig)
+{
+	ADS1118_Config_t configReg1118 = pConfig->config;
+	uint8_t PGA = configReg.pgaConfig;
+	float LSB;
+	switch (PGA)
+	{
+		case 0x0:{LSB=6.144/32768;} break;
+		case 0x1:{LSB=4.048/32768.;}break;
+		case 0x2:{LSB=2.048/32768.;}break;
+		case 0x3:{LSB=1.024/32768;}break;
+		case 0x4:{LSB=0.512/32768;}break;
+		default: {LSB=0.256/32768;};
+	}
+	float voltage = ((float)raw)*LSB;
+	return voltage;
+};
+
+
+
+
+
+float voltage_to_resistance(
+		float voltage,
+		float vRef,
+		float Res)
+{
+	float Th_resist=(voltage*Res*1000.)/( vRef - voltage);
+	return Th_resist;
+};
+
+
+
+float resistance_to_temperature(float resistance)
+{
+	float Beta=4381.5;
+	float R0 = 100000.;
+	float T0 = 25.+273.15;
+	float temperature =1/(1/Beta*log(resistance/R0)+1/(T0))-273.15;
+	return temperature;
+};
+
---
+++ ads1118.h
@@ -0,0 +1,115 @@
+﻿
+
+
+typedef struct{
+	OperatingMode_t 	operatingMode;
+	MultiplexerConfig_t channel;
+	PGA_Config_t 		pgaConfig;
+	MODEconfig_t 		modeConfig;
+	DataRate_t			dataRate;
+	ADC_TSmode_t		ADC_TSmode;
+	PULL_t 	   				pullMode;
+	Valid_t					validMode;
+	RDY_t    				readyMode;
+}ADS1118_Config_t;
+
+
+typedef enum {
+	CONV = 0x1,          
+    SCS = 0x1
+}OperatingMode_t;
+
+//MUX
+typedef enum{
+	AIN0_AIN1 = 0x0,
+	AIN0_AIN3 = 0x1,
+	AIN1_AIN3 = 0x2,
+	AIN2_AIN3 = 0x3,
+	AIN0_AIN_GND = 0x4,
+	AIN1_AIN_GND = 0x5,
+	AIN2_AIN_GND = 0x6,
+	AIN3_AIN_GND = 0x7
+}MultiplexerConfig_t;
+
+
+typedef enum
+{
+	FS6144 = 0x0,
+	FS4096 = 0x1,
+	FS2048 = 0x2,
+	FS1024 = 0x3,
+	FS0512 = 0x4,
+    FS0256 = 0x5
+} PGA_Config_t;
+
+
+//MODE
+typedef enum {
+	CCM = 0x0,//: Continuous-conversion mode
+	PDSSM = 0x1 //Single-shot mode or power-down state
+}MODEconfig_t;
+
+// Data rate
+typedef enum {
+	SPS8 = 0x0,
+	SPS16 = 0x1,
+	SPS32 = 0x2,
+	SPS64 = 0x3,
+	SPS128 = 0x4,
+	SPS250 = 0x5,
+	SPS475 = 0x6,
+	SPS860 = 0x7
+}DataRate_t;
+
+//mode(adc/temperature sensor)
+typedef enum
+{
+    ADC_MODE    			=   0x0,
+    TEMPERATURE_MODE 	=  	0x1
+} ADC_TSmode_t;
+
+
+//pull enable/disable
+typedef enum
+{
+    PULL_UP_DIS = 0x0,
+    PULL_UP_EN  = 0x1
+} PULL_t;
+//data valid
+typedef enum
+{
+    DATA_VALID      = 0x1,
+    DATA_INVALID    = 0x2
+} Valid_t;
+
+
+typedef enum
+{
+    DATA_READY 	= 0x0,
+    DATA_NREADY = 0x1
+} RDY_t;
+
+#define DEFAULT_PGA		PGA_2048
+
+void ads1118_config(void);
+
+void SPI_config(void);
+float ads1118_get_temperature(void);
+
+void ads1118_init(ADS_InitTypeDef* ConfigReg);
+
+uint16_t ads1118_convert(unsigned char channel);
+uint16_t ads1118_get_differential_0_1(uint8_t PGA);
+uint16_t ads1118_get_differential_2_3(uint8_t PGA);
+double	ads1118_get_differential_0_1_mv(uint8_t PGA);
+double	ads1118_get_differential_2_3_mv(uint8_t PGA);
+uint8_t SPI_send_Byte(uint8_t byte);
+uint16_t SPI_read_write_Reg(uint16_t CofigReg);
+uint16_t SPI_read_write_Byte(uint16_t TxData);
+void SPI_RCC_Configuration(void);
+void SPI_GPIO_Configuration(void);
+void SPI_NVIC_Configuration(void);
+int8_t  ads1118_get_temp_thermo_type_k( double input_voltage_mV, double input_cold_junction_C, double *output_hot_junction_C);
+uint16_t ads1118_median_filter(uint16_t *pbuffer);
+double ads1118_median_average_filter(double *pbuffer);
+#endif
